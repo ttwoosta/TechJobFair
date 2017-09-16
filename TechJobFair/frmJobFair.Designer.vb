59de2d71b960e915058e5d437c14cde7ec19f0ce
Tu Tong <ttwoosta@gmail.com>
2017-09-16 14:26:23 -0400

    Renamed main form.

---
+++ TechJobFair/frmJobFair.Designer.vb
@@ -0,0 +1,142 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmJobFair
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.picJobFair = New System.Windows.Forms.PictureBox()
+        Me.lblTitle = New System.Windows.Forms.Label()
+        Me.lblSubtitle = New System.Windows.Forms.Label()
+        Me.btnViewDetails = New System.Windows.Forms.Button()
+        Me.lblDate = New System.Windows.Forms.Label()
+        Me.lblLocation = New System.Windows.Forms.Label()
+        Me.lblRoom = New System.Windows.Forms.Label()
+        Me.btnExit = New System.Windows.Forms.Button()
+        CType(Me.picJobFair, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'picJobFair
+        '
+        Me.picJobFair.Location = New System.Drawing.Point(360, 29)
+        Me.picJobFair.Name = "picJobFair"
+        Me.picJobFair.Size = New System.Drawing.Size(297, 550)
+        Me.picJobFair.TabIndex = 0
+        Me.picJobFair.TabStop = False
+        '
+        'lblTitle
+        '
+        Me.lblTitle.AutoSize = True
+        Me.lblTitle.Font = New System.Drawing.Font("Tahoma", 22.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTitle.Location = New System.Drawing.Point(56, 49)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.Size = New System.Drawing.Size(246, 46)
+        Me.lblTitle.TabIndex = 1
+        Me.lblTitle.Text = "Tech Job Fair"
+        '
+        'lblSubtitle
+        '
+        Me.lblSubtitle.AutoSize = True
+        Me.lblSubtitle.Font = New System.Drawing.Font("Tahoma", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblSubtitle.Location = New System.Drawing.Point(60, 95)
+        Me.lblSubtitle.Name = "lblSubtitle"
+        Me.lblSubtitle.Size = New System.Drawing.Size(239, 24)
+        Me.lblSubtitle.TabIndex = 2
+        Me.lblSubtitle.Text = "All Students are Welcome"
+        '
+        'btnViewDetails
+        '
+        Me.btnViewDetails.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnViewDetails.Location = New System.Drawing.Point(93, 189)
+        Me.btnViewDetails.Name = "btnViewDetails"
+        Me.btnViewDetails.Size = New System.Drawing.Size(173, 34)
+        Me.btnViewDetails.TabIndex = 3
+        Me.btnViewDetails.Text = "View Job Fair Details"
+        Me.btnViewDetails.UseVisualStyleBackColor = True
+        '
+        'lblDate
+        '
+        Me.lblDate.AutoSize = True
+        Me.lblDate.Font = New System.Drawing.Font("Tahoma", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblDate.Location = New System.Drawing.Point(123, 333)
+        Me.lblDate.Name = "lblDate"
+        Me.lblDate.Size = New System.Drawing.Size(112, 23)
+        Me.lblDate.TabIndex = 4
+        Me.lblDate.Text = "May 2, 2016"
+        '
+        'lblLocation
+        '
+        Me.lblLocation.AutoSize = True
+        Me.lblLocation.Font = New System.Drawing.Font("Tahoma", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblLocation.Location = New System.Drawing.Point(80, 391)
+        Me.lblLocation.Name = "lblLocation"
+        Me.lblLocation.Size = New System.Drawing.Size(199, 23)
+        Me.lblLocation.TabIndex = 5
+        Me.lblLocation.Text = "Located in Lochlan Hal"
+        '
+        'lblRoom
+        '
+        Me.lblRoom.AutoSize = True
+        Me.lblRoom.Font = New System.Drawing.Font("Tahoma", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblRoom.Location = New System.Drawing.Point(132, 423)
+        Me.lblRoom.Name = "lblRoom"
+        Me.lblRoom.Size = New System.Drawing.Size(94, 23)
+        Me.lblRoom.TabIndex = 6
+        Me.lblRoom.Text = "Room 101"
+        '
+        'btnExit
+        '
+        Me.btnExit.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.Location = New System.Drawing.Point(107, 543)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(139, 36)
+        Me.btnExit.TabIndex = 7
+        Me.btnExit.Text = "Exit Window"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'frmC2P1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(669, 601)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.lblRoom)
+        Me.Controls.Add(Me.lblLocation)
+        Me.Controls.Add(Me.lblDate)
+        Me.Controls.Add(Me.btnViewDetails)
+        Me.Controls.Add(Me.lblSubtitle)
+        Me.Controls.Add(Me.lblTitle)
+        Me.Controls.Add(Me.picJobFair)
+        Me.Name = "frmC2P1"
+        CType(Me.picJobFair, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents picJobFair As PictureBox
+    Friend WithEvents lblTitle As Label
+    Friend WithEvents lblSubtitle As Label
+    Friend WithEvents btnViewDetails As Button
+    Friend WithEvents lblDate As Label
+    Friend WithEvents lblLocation As Label
+    Friend WithEvents lblRoom As Label
+    Friend WithEvents btnExit As Button
+End Class
---
+++ TechJobFair/frmJobFair.vb
@@ -0,0 +1,3 @@
+﻿Public Class frmJobFair
+
+End Class
